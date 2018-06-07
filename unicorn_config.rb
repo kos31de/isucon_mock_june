9cef8a42d754757f1e09a0b889e043cf44f0a2db
root <root@ip-172-31-36-174.ap-northeast-1.compute.internal>
2018-06-07 18:28:05 +0900

    ファイルのpermissionの変更

--- unicorn_config.rb
+++ unicorn_config.rb
@@ -2,9 +2,9 @@
 worker_processes 4
 preload_app true
 # listen "127.0.0.1:8080"
-listen "/home/isucon/private_isu/webapp/ruby/tmp/unicorn_config.sock", backlog: 1024
 @dir = "/home/isucon/private_isu/webapp/ruby/"
 
+listen "#{@dir}/tmp/unicorn_config.sock", backlog: 64
 #worker_processes 2 # CPUのコア数に揃える
 working_directory @dir
 
